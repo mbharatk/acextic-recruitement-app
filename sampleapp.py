6d8b0a7a3b8e9ca7e6c96578acf5e79c4f4d4fe6
Bharat Kumar Mallavarapu <ybharatk7@gmail.com>
2024-10-19 15:25:51 -0500

    Update sampleapp.py

--- sampleapp.py
+++ sampleapp.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
-pip install matplotlib
+#pip install matplotlib
 import matplotlib.pyplot as plt
-pip install plotly
+#pip install plotly
 import plotly.express as px
 from io import StringIO
 import os
